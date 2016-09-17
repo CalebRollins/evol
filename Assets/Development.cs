7dc07a291ca94bbf472beace8e0e88a8536682b2
Caleb Rollins <Caleb Rollins>
2016-09-17 13:51:26 -0500

    Branching and angle final

--- Assets/Development.cs
+++ Assets/Development.cs
@@ -14,22 +14,20 @@ public static class Development
         int scaleFactor = biomorph.dna.genes[2];
         Vector3 scale = new Vector3((10F - scaleFactor) / 2F, (scaleFactor + 1) / 2F, (10F - scaleFactor) / 2F);
         GameObject go = GameObject.Instantiate(biomorph.prefab, biomorph.transform.position, Quaternion.identity) as GameObject;
-        GameObject goToInstantiate = GameObject.Instantiate(biomorph.prefab, Vector3.zero, Quaternion.identity) as GameObject;
-        goToInstantiate.transform.localScale = scale;
 
         Initialize(go, biomorph.gameObject, color);
-        Fractal(go, levels, biomorph, color, goToInstantiate);
+        Fractal(go, levels, biomorph, color);
 
     }
 
-    public static void Fractal (GameObject parent, int levels, Biomorph biomorph, Color color, GameObject goToInstantiate)
+    public static void Fractal (GameObject parent, int levels, Biomorph biomorph, Color color)
     {
         if (levels <= 0) return;
 
         levels--;
 
-        GameObject go = GameObject.Instantiate(goToInstantiate, Vector3.zero, Quaternion.identity) as GameObject;
-        GameObject go2 = GameObject.Instantiate(goToInstantiate, Vector3.zero, Quaternion.identity) as GameObject;
+        GameObject go = GameObject.Instantiate(biomorph.prefab, Vector3.zero, Quaternion.identity) as GameObject;
+        GameObject go2 = GameObject.Instantiate(biomorph.prefab, Vector3.zero, Quaternion.identity) as GameObject;
 
         Initialize(go, parent, color);
         Initialize(go2, parent, color);
@@ -38,12 +36,12 @@ public static class Development
         go.transform.position = spawnPoint;
         go2.transform.position = spawnPoint;
 
-        int angle = (biomorph.dna.genes[1] + 1) * 10;
+        int angle = (biomorph.dna.genes[1] + 1) * 9;
         go.transform.localRotation = Quaternion.Euler(0f, 0f, -(float)angle);
         go2.transform.localRotation = Quaternion.Euler(0f, 0f, (float)angle);
 
-        Fractal(go, levels, biomorph, color, goToInstantiate);
-        Fractal(go2, levels, biomorph, color, goToInstantiate);
+        Fractal(go, levels, biomorph, color);
+        Fractal(go2, levels, biomorph, color);
     }
 
     public static void Initialize(GameObject go, GameObject parent, Color color)
