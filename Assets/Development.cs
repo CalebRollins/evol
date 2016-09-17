95de824f217f604d21a4b328ef7b217f5263945c
Caleb Rollins <Caleb Rollins>
2016-09-16 21:22:01 -0500

    More scale work

--- Assets/Development.cs
+++ Assets/Development.cs
@@ -8,57 +8,28 @@ public static class Development
         int levels = biomorph.dna.genes[0];
         
         int[] genes = biomorph.dna.genes;
-        GameObject go = GameObject.Instantiate(biomorph.prefab, biomorph.transform.position, Quaternion.identity) as GameObject;
+        
         Color color = new Color((float)genes[0] / (DNA.geneMaxVal + 1F), (float)genes[1] / (DNA.geneMaxVal + 1F), (float)genes[2] / (DNA.geneMaxVal + 1F));
 
         int scaleFactor = biomorph.dna.genes[2];
         Vector3 scale = new Vector3((10F - scaleFactor) / 2F, (scaleFactor + 1) / 2F, (10F - scaleFactor) / 2F);
-        go.transform.localScale = scale;
+        GameObject go = GameObject.Instantiate(biomorph.prefab, biomorph.transform.position, Quaternion.identity) as GameObject;
+        GameObject goToInstantiate = GameObject.Instantiate(biomorph.prefab, Vector3.zero, Quaternion.identity) as GameObject;
+        goToInstantiate.transform.localScale = scale;
 
         Initialize(go, biomorph.gameObject, color);
-        Fractal(go, levels, biomorph, color, scale);
+        Fractal(go, levels, biomorph, color, goToInstantiate);
 
-        //ChangeParentScale(go.transform, scale);
-        //Scale(go, scale);
     }
 
-    // scale each cylinder in the biomorph
-    // have to go from the bottom up and disparent all children so they don't inherit the scale
-    public static void Scale(GameObject go, Vector3 scale)
-    {
-        foreach (Transform child in go.transform)
-        {
-            if (go.tag == "Cylinder")
-            {
-                ChangeParentScale(go.transform, scale);
-                Scale(child.gameObject, scale);
-            }
-        }
-    }
-
-    private static void ChangeParentScale(Transform parent, Vector3 scale)
-    {
-        List<Transform> children = new List<Transform>();
-        foreach (Transform child in parent)
-        {
-            child.SetParent(null);
-            children.Add(child);
-        }
-        parent.localScale = scale;
-        foreach (Transform child in children) child.SetParent(parent, true);
-    }
-
-    public static void Fractal (GameObject parent, int levels, Biomorph biomorph, Color color, Vector3 scale)
+    public static void Fractal (GameObject parent, int levels, Biomorph biomorph, Color color, GameObject goToInstantiate)
     {
         if (levels <= 0) return;
 
         levels--;
 
-        GameObject go = GameObject.Instantiate(biomorph.prefab, Vector3.zero, Quaternion.identity) as GameObject;
-        GameObject go2 = GameObject.Instantiate(biomorph.prefab, Vector3.zero, Quaternion.identity) as GameObject;
-
-        go.transform.localScale = scale;
-        go2.transform.localScale = scale;
+        GameObject go = GameObject.Instantiate(goToInstantiate, Vector3.zero, Quaternion.identity) as GameObject;
+        GameObject go2 = GameObject.Instantiate(goToInstantiate, Vector3.zero, Quaternion.identity) as GameObject;
 
         Initialize(go, parent, color);
         Initialize(go2, parent, color);
@@ -71,13 +42,13 @@ public static class Development
         go.transform.localRotation = Quaternion.Euler(0f, 0f, -(float)angle);
         go2.transform.localRotation = Quaternion.Euler(0f, 0f, (float)angle);
 
-        Fractal(go, levels, biomorph, color, scale);
-        Fractal(go2, levels, biomorph, color, scale);
+        Fractal(go, levels, biomorph, color, goToInstantiate);
+        Fractal(go2, levels, biomorph, color, goToInstantiate);
     }
 
     public static void Initialize(GameObject go, GameObject parent, Color color)
     {
-        go.transform.SetParent(parent.transform, true);
+        go.transform.parent = parent.transform;
         
         GameObject model = go.transform.GetChild(0).gameObject;
         model.GetComponent<MeshRenderer>().material.color = color;
